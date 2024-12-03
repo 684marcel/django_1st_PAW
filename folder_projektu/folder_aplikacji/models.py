2bd4ab87512be91155deb157a33338337a549843
684marcel <mmichalczyk1@st.swps.edu.pl>
2024-12-03 11:44:23 +0100

    3.12 - stanowiska, osoby

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+
+PLCIE = models.IntegerChoices('PLEC', 'Kobieta Mężczyzna Inna')
 
 SHIRT_SIZES = (
         ('S', 'Small'),
@@ -30,3 +32,37 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+   
+    
+class Osoba(models.Model):
+    PLEC_CHOICES = (
+        ("K", "Kobieta"),
+        ("M", "Mężczyzna"),
+        ("I", "Inna"),
+    )
+    
+    imie = models.CharField(max_length=40, blank = False, null = False)
+    nazwisko = models.CharField(max_length=60, blank = False, null = False)
+    plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
+    stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+    data_dodania = models.DateField(default = date.today, blank=False, null=False)
+    
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}' 
+    
+    class Meta:
+        ordering = ["nazwisko"]
+        verbose_name = "Osoba"
+        verbose_name_plural = "Osoby"
+        
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=80, blank = False, null = False)
+    opis = models.TextField(blank = False, null = False)
+    
+    def __str__(self):
+        return self.nazwa
+    
+    class Meta:
+        verbose_name = "Stanowisko"
+        verbose_name_plural = "Stanowiska"
